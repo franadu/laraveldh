3f48cfda54051d530dcbf1504be8e7d692514148
AndresMndez <andresmndeze@gmail.com>
2018-12-07 11:24:15 -0300

    Home estable

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@
 Route::get('api/categoryproduct','CategoryController@apiCategoryProduct');
 Route::get('/api/products','ProductController@apiIndex');
 Route::get('/api/categories','CategoryController@apiIndex');
-
+Route::get('/', 'IndexController@index');
 <<<<<<< HEAD
 =======
 
@@ -33,18 +33,3 @@ Route::get('preguntas-frecuentes', function(){
 Route::get('{name}','CategoryController@show');
 
 Route::get('{name}/{id}','PurchaseController@show');
-
-
-
-<<<<<<< HEAD
-Route::get('/', 'IndexController@index');
-
-=======
-Route::get('/', function(){
-	return view('principal.index');
-});
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
->>>>>>> bd9e9e52b27c325ae8c8f177c6445ebeb3a55fc4
